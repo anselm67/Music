6ad78137c7476a7a783795e5b9a9781d4500d522
anselm <abairdsmith@gmail.com>
2026-05-15 18:06:27 +0200

    staffer: switch to from bf16-mixed to 16-mixed. This should alleviate the loss plateau-ing, or will it?

--- src/models/staffer_module.py
+++ src/models/staffer_module.py
@@ -94,6 +94,7 @@ class StafferModule(L.LightningModule):
             self.parameters(),
             lr=self.config.lr,
             weight_decay=self.config.weight_decay,
+            eps=1e-4,
         )
 
         def lr_lambda(step: int) -> float:
